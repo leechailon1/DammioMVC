4ae0d0a784316de0e3a191fb009efb235c93dd1b
leechailon1 <leechailon1@gmail.com>
2021-05-12 10:44:21 +0700

    Initial Project

---
+++ DammioMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DammioMVC.Startup))]
+namespace DammioMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
